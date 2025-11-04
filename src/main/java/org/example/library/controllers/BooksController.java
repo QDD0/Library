455b1399b50e116791a34fc8c2efe80fb362f07c
Den <dend51108@gmail.com>
2025-11-04 13:03:07 +0300

    + отображение данных с book

--- src/main/java/org/example/library/controllers/BooksController.java
+++ src/main/java/org/example/library/controllers/BooksController.java
@@ -1,4 +1,26 @@
 package org.example.library.controllers;
 
+import org.example.library.dao.BookDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/books")
 public class BooksController {
+
+    private final BookDAO bookDAO;
+
+    @Autowired
+    public BooksController(BookDAO bookDAO) {
+        this.bookDAO = bookDAO;
+    }
+
+    @GetMapping("")
+    public String showAllBooks(Model model) {
+        model.addAttribute("booksList", bookDAO.showAllBooks());
+        return "book/homeBooks";
+    }
 }
--- src/main/java/org/example/library/controllers/PeopleController.java
+++ src/main/java/org/example/library/controllers/PeopleController.java
@@ -1,4 +1,14 @@
 package org.example.library.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/people")
 public class PeopleController {
+    @GetMapping("")
+    public String showAllPeople() {
+        return "people/homePeople";
+    }
 }
